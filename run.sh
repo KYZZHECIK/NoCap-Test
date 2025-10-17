d5a2b52ae6f16d3531a351a6fdb0e73b1f281b9b
Ahmadov Avazagha <ahmadov.avazagha@gmail.com>
2025-10-17 13:16:56 +0200

    Dealing with more consequences of being poor

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@ torchrun --standalone --nproc_per_node=1 train_gpt2.py \
   --input_val_bin "data/fineweb10B/fineweb_val_*.bin" \
   --output_dir pylog124M \
   --model d12 \
-  --batch_size 16 \
-  --grad_accumulation_steps 32 \
+  --batch_size 8 \
+  --grad_accumulation_steps 64 \
   --sequence_length 1024 \
   --val_loss_every 128 \
   --val_batch_size 16 \
